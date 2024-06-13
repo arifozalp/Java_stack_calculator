81b7051627a1ea4bd02d42552c321b95ee9dfee7
arifozalp <114351791+arifozalp@users.noreply.github.com>
2024-06-13 23:48:41 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     
     public static void main(String[] args) {
-        calculator c=new calculator("C:\\Users\\Arif Özalp\\Desktop\\infix.txt");
+        calculator c=new calculator("C:\\Users\\Arif Özalp\\Desktop\\infix.txt"); //You have to change this line according to your infix file path.
     }
 }
